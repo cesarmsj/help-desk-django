0b2fff4d27fd07ba38cbe9f1a78eb1cd37fc3544
cesarmsj <cesarsj@unipam.edu.br>
2021-06-27 12:49:17 -0300

    teste

--- README.md
+++
@@ -1,2 +0,0 @@
-# help-desk-django
-Sistema de Help Desk desenvolvido com Django
--- help_desk_django_project/helpdesk/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-#from help_desk_django_project.helpdesk.models import Atendente
-
-# Register your models here.
-
-#class AtendenteAdmin(admin.ModelAdmin):
-#    pass
-#admin.site.register(Atendente, AtendenteAdmin)
